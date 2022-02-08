a81d9557010255cb7567a4214566a14d8e35634f
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-02-08 16:06:55 +0530

    Created dedicated file to map files to their corresponding gists

--- src/library/config.ts
+++ src/library/config.ts
@@ -1,10 +1,18 @@
 //  Library
 import * as core from '@actions/core'
+import * as path from 'node:path'
+import * as yaml from 'js-yaml'
 
 //  ======
 //  CONFIG
 //  ======
 
+const workspaceURL = process.env.GITHUB_WORKSPACE || ''
+
+
 export const isDryRun = core.getBooleanInput('dryrun') ?? false
 
-export const gists = core.getInput('gists')
+const fileName = core.getInput('gists')
+const fileContents = path.join(workspaceURL, '.github', fileName)
+
+export const gists = yaml.load(fileContents)
