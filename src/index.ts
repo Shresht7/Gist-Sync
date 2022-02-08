2110690901ed3c3153fa4abe4d9a881e44797d3b
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-02-08 17:44:00 +0530

    Testing it live

--- src/index.ts
+++ src/index.ts
@@ -4,24 +4,26 @@ import * as github from '@actions/github'
 import { octokit } from './library/octokit'
 import { isDryRun, gists } from './library/config'
 
-try {
-    for (const gist of gists) {
-        console.log(gist)
-        console.log({
-            gist_id: gist.gistID,
-            files: gist.files
-        })
-        // octokit.rest.gists.get({
-        //     gist_id: gist.gistID,
-        // })
-        //     .then((res) => console.log(res.data.description))
-        //     .catch(err => console.error(err))
-        // octokit.rest.gists.update({
-        //     gist_id: gist.gistID,
-        //     files: gist.files
-        // })
+
+async function run() {
+    try {
+        for (const gist of gists) {
+            const existingGist = await octokit.rest.gists.get({
+                gist_id: gist.gistID,
+            })
+            if (existingGist) {
+                octokit.rest.gists.update({
+                    gist_id: gist.gistID,
+                    files: gist.files
+                })
+            } else {
+                console.warn("Gist does not exist")
+            }
+        }
+    } catch (err) {
+        let error = err as Error
+        core.setFailed(error.message)
     }
-} catch (err) {
-    let error = err as Error
-    core.setFailed(error.message)
 }
+
+run()
